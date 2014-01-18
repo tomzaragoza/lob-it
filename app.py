96ae1575c3659f82dd3ab85c01410790aebea7bb
Tom Zaragoza <tomdzaragoza@gmail.com>
2014-01-18 09:34:46 -0500

    Add in to and from longitude and lattitude

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import lob
+import geopy
 from forms import ApiKey
 from flask import *
 from pymongo import MongoClient
@@ -22,24 +23,51 @@ def home():
 def get_post_cards():
 	pass
 
+def get_lat_long(city, state, address):
+	# coords = {'lat': None, 'lng': None}
+	# pass
+	from geopy.geocoders import GeocoderDotUS
+	f_string = "%s {0} {1}".format(city, state)
+	print f_string
+	geolocator = GeocoderDotUS(format_string=f_string)
+	print 'this is the address', address
+	address, (latitude, longitude) = geolocator.geocode(address)
+	return (address, latitude, longitude)
+# 	>>> from geopy.geocoders import GeocoderDotUS
+# >>> geolocator = GeocoderDotUS(format_string="%s, Cleveland OH")
+# >>> address, (latitude, longitude) = geolocator.geocode("11111 Euclid Ave")
+# >>> print(address, latitude, longitude)
+
 @app.route('/api_key', methods=['POST'])
 def add_api_key():
 	""" Once the button was pressed for API key getting passed in,
-		we verify, pull in data""" 
+		we verify, pull in data.
+	""" 
 	if request.method == 'POST':
 		form = request.form
 		lob.api_key = str(form['apikey'])
 		pc = []
 		postcards = lob.Postcard.list()
-		print dir(postcards[0])
 		for p in postcards:
-			to_address = '{0}, {1}, {2}, {3}'.format(p.to.address_line1, p.to.address_city, p.to.address_state, p.to.address_country)
-			from_address = '{0}, {1}, {2}, {3}'.format(p.from_address.address_line1, p.from_address.address_city, p.from_address.address_state, p.from_address.address_country)
-			pc.append({
-						'name': p.name, 
-						'to': to_address,
-						'from': from_address
-					}) 
+			
+			try:
+				to_address = '{0}, {1}, {2}, {3}'.format(p.to.address_line1, p.to.address_city, p.to.address_state, p.to.address_country)
+				from_address = '{0}, {1}, {2}, {3}'.format(p.from_address.address_line1, p.from_address.address_city, p.from_address.address_state, p.from_address.address_country)
+				to_lat_long = get_lat_long(p.to.address_city, p.to.address_state, p.to.address_line1)
+				from_lat_long = get_lat_long(p.from_address.address_city, p.from_address.address_state, p.from_address.address_line1)
+				pc.append({
+							'name': p.name, 
+							'to': to_address,
+							'to_city': p.to.address_city,
+							'from': from_address,
+							'from_city': p.from_address.address_city,
+							'to_latitude': to_lat_long[1],
+							'to_longitude': to_lat_long[2],
+							'from_latitude': from_lat_long[1],
+							'from_longitude': from_lat_long[2]
+						}) 
+			except:
+				pass # the address that was used isn't real
 		
 		return render_template('map.html', hi='hi', postcards=pc)
 
---
+++ postcards.py
@@ -0,0 +1,4 @@
+import lob
+lob.api_key = 'test_4a06255aa2fb75897597e5851267ea2a199'
+
+l = lob.Postcard.list()
