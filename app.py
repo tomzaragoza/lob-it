c06d3b0f4bafd2506bdf7c12ceda03f4bd67af8e
Tom Zaragoza <tomdzaragoza@gmail.com>
2014-01-18 12:35:12 -0500

    Add in markers, better positioning

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ app.config.from_object('config')
 
 mongo = MongoClient()
 db = mongo['lob']
-collection = db['postcards']
-lob.api = ''
+postcards_collection = db['postcards']
+settings_collection = db['settings']
+# lob_obj.api_key = ''
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
@@ -32,7 +33,7 @@ def get_lat_long(city, state, address):
 	address, (latitude, longitude) = geolocator.geocode(address)
 	return (address, latitude, longitude)
 
-@app.route('/api_key', methods=['POST'])
+@app.route('/map', methods=['POST', 'GET'])
 def add_api_key():
 	""" Once the button was pressed for API key getting passed in,
 		we verify, pull in data.
@@ -40,30 +41,36 @@ def add_api_key():
 	if request.method == 'POST':
 		form = request.form
 		lob.api_key = str(form['apikey'])
-		pc = []
-		postcards = lob.Postcard.list()
-		for p in postcards:
-			
-			try:
-				to_address = '{0}, {1}, {2}, {3}'.format(p.to.address_line1, p.to.address_city, p.to.address_state, p.to.address_country)
-				from_address = '{0}, {1}, {2}, {3}'.format(p.from_address.address_line1, p.from_address.address_city, p.from_address.address_state, p.from_address.address_country)
-				to_lat_long = get_lat_long(p.to.address_city, p.to.address_state, p.to.address_line1)
-				from_lat_long = get_lat_long(p.from_address.address_city, p.from_address.address_state, p.from_address.address_line1)
-				pc.append({
-							'name': p.name, 
-							'to': to_address,
-							'to_city': p.to.address_city,
-							'from': from_address,
-							'from_city': p.from_address.address_city,
-							'to_latitude': to_lat_long[1],
-							'to_longitude': to_lat_long[2],
-							'from_latitude': from_lat_long[1],
-							'from_longitude': from_lat_long[2]
-						}) 
-			except:
-				pass # the address that was used isn't real
+
+		if not settings_collection.find_one():
+			settings_collection.insert({'api-key': lob.api_key}, upsert=True)
+	elif request.method == 'GET':
+		lob.api_key = settings_collection.find_one({})['api-key']
+
+	pc = []
+	postcards = lob.Postcard.list()
+	for p in postcards:
 		
-		return render_template('map.html', hi='hi', postcards=json.dumps({'data': pc}))
+		try:
+			to_address = '{0}, {1}, {2}, {3}'.format(p.to.address_line1, p.to.address_city, p.to.address_state, p.to.address_country)
+			from_address = '{0}, {1}, {2}, {3}'.format(p.from_address.address_line1, p.from_address.address_city, p.from_address.address_state, p.from_address.address_country)
+			to_lat_long = get_lat_long(p.to.address_city, p.to.address_state, p.to.address_line1)
+			from_lat_long = get_lat_long(p.from_address.address_city, p.from_address.address_state, p.from_address.address_line1)
+			pc.append({
+						'name': p.name, 
+						'to': to_address,
+						'to_city': p.to.address_city,
+						'from': from_address,
+						'from_city': p.from_address.address_city,
+						'to_latitude': to_lat_long[1],
+						'to_longitude': to_lat_long[2],
+						'from_latitude': from_lat_long[1],
+						'from_longitude': from_lat_long[2]
+					}) 
+		except:
+			pass # the address that was used isn't real
+		
+		return render_template('map.html', postcards=json.dumps({'data': pc}))
 
 @app.route('/map')
 def map():
