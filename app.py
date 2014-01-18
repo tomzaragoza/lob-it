ccb5632e74ffd544a04044cdcdc5f5241b5d1348
Tom Zaragoza <tomdzaragoza@gmail.com>
2014-01-17 23:28:40 -0500

    just simple page

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import lob
 from flask import *
 from pymongo import MongoClient
+
+
+app = Flask(__name__)
+
+mongo = MongoClient()
+db = mongo['lob']
+collection = db['postcards']
+
+@app.route('/')
+def hello():
+	return render_template('index.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run()
